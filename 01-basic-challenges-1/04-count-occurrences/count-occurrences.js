9b337654c3d8ac3b120e8abc69fb99e19dbed341
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-28 22:46:18 +0530

    04 challenge complete

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,9 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  const obj = {};
+  for (let i = 0; i <= str.length; i++) {
+    obj[str[i]] = (obj[str[i]] || 0) + 1;
+  }
+  return obj[char];
+}
 
 module.exports = countOccurrences;
