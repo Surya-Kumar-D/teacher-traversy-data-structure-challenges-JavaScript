8a119114b9c39011447779a28ea336e3d35a561b
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-28 21:40:20 +0530

    03 challenge complete

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+
+    case "-":
+      return a - b;
+
+    case "*":
+      return a * b;
+
+    case "/":
+      return a / b;
+    default:
+      throw new Error("Invalid operator");
+  }
+}
 
 module.exports = calculator;
