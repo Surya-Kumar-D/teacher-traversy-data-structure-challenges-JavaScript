1e2a7b22bb88fa4af2b9dc2733c34e8fece31458
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-29 20:47:19 +0530

    08 challenge complete

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,11 @@
-function isPalindrome() {}
+// using inbuilt reverse;
+// function isPalindrome(str) {
+//   return str.split(" ").reverse().join(" ") === str;
+// }
+
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return cleanedStr.split("").reverse().join("") === cleanedStr;
+}
 
 module.exports = isPalindrome;
