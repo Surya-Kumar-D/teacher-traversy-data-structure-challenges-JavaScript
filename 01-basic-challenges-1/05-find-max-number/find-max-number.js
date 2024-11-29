7109572f13875faed701cbdf42efb80407bc9052
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-29 19:21:42 +0530

    o5 challenge complete

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,15 @@
-function findMaxNumber() {}
+// "using Math.Max Method";
+// function findMaxNumber(arr) {
+//   return Math.max(...arr);
+// }
 
+// Using For loop
+
+function findMaxNumber(arr) {
+  let currentElement = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > currentElement) currentElement = arr[index];
+  }
+  return currentElement;
+}
 module.exports = findMaxNumber;
