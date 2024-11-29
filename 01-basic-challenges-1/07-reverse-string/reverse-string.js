637086af166b9128ea9c58668ba7f4922e5cfbff
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-29 20:15:22 +0530

    07 challenge complete

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversed = "";
+  for (let index = str.length - 1; index >= 0; index--) {
+    const element = str[index];
+    reversed += element;
+  }
+  return reversed;
+}
 
 module.exports = reverseString;
