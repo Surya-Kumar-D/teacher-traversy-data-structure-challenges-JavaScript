6080457b53b3f99dc3d490179777eb427c36f79f
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-01 18:23:07 +0530

    challenge 09 complete

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,24 @@
-function countVowels() {}
+function countVowels(str) {
+  let count = 0;
+  for (key of str) {
+    switch (key) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+      case "A":
+      case "E":
+      case "I":
+      case "O":
+      case "U":
+        count++;
+        break;
+      default:
+        break;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
