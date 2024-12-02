9d51ae06c7271253e6cfa3fc5a311ea71a1a5e67
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-02 18:07:32 +0530

    challenge 02 complete'

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
-
+// function arrayIntersection(arr1, arr2) {
+//   const intersectedElements = [];
+//   for (let index = 0; index < arr1.length; index++) {
+//     const element = arr1[index];
+//     if (arr2.includes(element)) {
+//       intersectedElements.push(element);
+//     }
+//   }
+//   return intersectedElements;
+// }
+function arrayIntersection(arr1, arr2) {
+  const set2 = new Set(arr2);
+  return arr1.filter((el) => set2.has(el));
+}
 module.exports = arrayIntersection;
