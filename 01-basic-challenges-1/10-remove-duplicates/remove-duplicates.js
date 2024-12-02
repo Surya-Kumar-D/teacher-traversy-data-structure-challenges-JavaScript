55fcb5a9e1721454542994d4db91d9b743d5a92f
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-02 09:32:00 +0530

    challenge 10 complete - basic challenges 01 complete

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+//   const removedArr = new Set(arr);
+//   return [...removedArr];
+// }
+
+function removeDuplicates(arr) {
+  const uniqueArr = [];
+  for (let index = 0; index < arr.length; index++) {
+    if (!uniqueArr.includes(arr[index])) {
+      uniqueArr.push(arr[index]);
+    }
+  }
+  return uniqueArr;
+}
 
 module.exports = removeDuplicates;
