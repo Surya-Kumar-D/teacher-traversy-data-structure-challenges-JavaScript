a812f9daca770fae318678cbde44be1265aa63d2
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 21:00:11 +0530

    challenge 06 complete

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,24 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//   const newSet = new Set(str);
+//   for (let el of str) {
+//     if (newSet.has(el)) {
+//       newSet.delete(el);
+//     } else {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
+function areAllCharactersUnique(str) {
+  const obj = {};
+
+  for (let el of str) {
+    obj[el] = (obj[el] | 0) + 1;
+  }
+  for (let el of Object.values(obj)) {
+    if (el > 1) return false;
+  }
+  return true;
+}
 module.exports = areAllCharactersUnique;
