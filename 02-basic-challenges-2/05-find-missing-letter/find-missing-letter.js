c2e7fe3ebf4ae5ce9997c3f988af02301ff555c1
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 13:29:00 +0530

    challenge 05 complete

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(array) {
+  const start = array[0].charCodeAt(0);
+  let result = "";
+  for (
+    let index = start;
+    index <= array[array.length - 1].charCodeAt(0);
+    index++
+  ) {
+    console.log(index);
+    if (!array.includes(String.fromCharCode(index))) {
+      result = String.fromCharCode(index);
+    }
+  }
+  return result;
+}
 
 module.exports = findMissingLetter;
