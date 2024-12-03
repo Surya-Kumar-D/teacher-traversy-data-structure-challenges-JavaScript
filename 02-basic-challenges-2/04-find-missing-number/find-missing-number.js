cf8ff537ca323eb7274e709c50780f0b886650c4
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 07:49:26 +0530

    challenge 04 complete

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,14 @@
-function findMissingNumber() {}
+function findMissingNumber(array) {
+  if (array.length === 0) return 1;
+  let missingNum = 0;
+  console.log(array.includes(1));
+  for (let index = 1; index <= array.length; index++) {
+    if (!array.includes(index)) {
+      missingNum = index;
+      //   break;
+    }
+  }
+  return missingNum;
+}
 
 module.exports = findMissingNumber;
