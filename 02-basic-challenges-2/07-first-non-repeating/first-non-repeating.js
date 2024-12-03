c02e5ea810adb058cf135a3dd1bb39e5f5598446
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-03 21:05:39 +0530

    challenge 07 complete

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,14 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const obj = {};
+  for (let el of str) {
+    obj[el] = (obj[el] | 0) + 1;
+  }
+  for (let char of str) {
+    if (obj[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
