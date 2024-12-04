d70b3f307b20413dca70b0a9b56ada7024f00957
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-04 20:22:57 +0530

    challenge 08 complete

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,20 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+  const results = [];
+  for (let i = 0; i < num; i++) {
+    const random1 = Math.floor(Math.random() * 6) + 1;
+    const random2 = Math.floor(Math.random() * 6) + 1;
+    let result = "";
+    const sumRandoms = random1 + random2;
+    if (sumRandoms === 7 || sumRandoms === 11) {
+      result = "win";
+    } else if (sumRandoms === 2 || sumRandoms === 3 || sumRandoms === 12) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+    results.push({ dice1: random1, dice2: random2, sum: sumRandoms, result });
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
