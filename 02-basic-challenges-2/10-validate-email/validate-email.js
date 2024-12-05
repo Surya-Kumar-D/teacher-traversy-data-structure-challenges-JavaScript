1896aa9ff587d32afca8d9400ca56cc478302d26
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-05 12:40:20 +0530

    challenge 10 complete - basic challenge part 2 complete

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,10 @@
-function validateEmail() {}
+function validateEmail(email) {
+  if (email.indexOf("@") === -1) return false;
+  const [localPart, domainPart] = email.split("@");
+  if (localPart.length === 0 || domainPart.length < 3) return false;
+  const domainExtension = domainPart.split(".");
+  if (domainExtension.length < 2 || domainExtension[1].length < 2) return false;
+  return true;
+}
 
 module.exports = validateEmail;
