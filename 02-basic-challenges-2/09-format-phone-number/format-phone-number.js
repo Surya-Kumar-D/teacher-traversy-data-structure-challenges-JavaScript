268dbe5d95c75c4a899109bb3c2c2826318bb2e3
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-05 08:45:16 +0530

    challenge 09 complete

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numArr) {
+  if (numArr.length < 10) return "Invalid Numbers. Need 10 Numbers";
+  const areaCode = numArr.slice(0, 3).join("");
+  const prefix = numArr.slice(3, 6).join("");
+  const lineNumber = numArr.slice(6).join("");
+  return `(${areaCode}) ${prefix}-${lineNumber}`;
+}
 
 module.exports = formatPhoneNumber;
