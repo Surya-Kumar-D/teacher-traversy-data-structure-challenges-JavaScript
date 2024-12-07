b1e34b739aa473e31c786173f80674cd3af9a9bd
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-07 18:23:27 +0530

    complete challenge 03 calulate-total-sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,11 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(objArr, tax) {
+  const totalPrice = objArr.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  );
+  const afterTaxPrice = (totalPrice * tax) / 100 + totalPrice;
+
+  return afterTaxPrice;
+}
 
 module.exports = calculateTotalSalesWithTax;
