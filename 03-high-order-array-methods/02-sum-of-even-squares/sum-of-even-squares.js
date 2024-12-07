ec44e3190947bd65a99799c5272c71c2d4f97ec2
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-07 16:37:12 +0530

    challenge 02 complete sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,7 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numArr) {
+  return numArr
+    .filter((num) => num % 2 == 0)
+    .reduce((acc, curr) => acc + curr * curr, 0);
+}
 
 module.exports = sumOfEvenSquares;
