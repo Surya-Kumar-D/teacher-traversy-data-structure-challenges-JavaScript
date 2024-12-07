70afe662d6357a78eb8c0180f19aa48dac236c1c
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-07 20:49:44 +0530

    challenge 04 highest-scoring-word complete

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,10 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const splitStr = str.toLowerCase().split(" ");
+  const scores = splitStr.map((alphabet) =>
+    [...alphabet].reduce((acc, curr) => acc + curr.charCodeAt(0) - 96, 0)
+  );
+  const highestScore = scores.indexOf(Math.max(...scores));
+  return splitStr[highestScore];
+}
 
 module.exports = highestScoringWord;
