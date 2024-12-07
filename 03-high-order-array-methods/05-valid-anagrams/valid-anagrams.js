9bbf38ab9a7c4c879274ceed488ef52f3e4de9ff
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-07 22:12:10 +0530

    challenge 05 complete valid-anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,16 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  let obj1 = {};
+  let obj2 = {};
+  for (let str of str1) {
+    obj1[str] = (obj1[str] || 0) + 1;
+  }
+  for (let str of str2) {
+    obj2[str] = (obj2[str] || 0) + 1;
+  }
+  for (let str of Object.keys(obj1)) {
+    if (obj1[str] !== obj2[str]) return false;
+  }
+  return true;
+}
 
 module.exports = validAnagrams;
