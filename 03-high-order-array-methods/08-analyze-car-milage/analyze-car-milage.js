d016c98a11ee4abd84a2ec8241625e13862c4fa9
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-09 22:14:15 +0530

    challenge 08 complete

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,13 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((acc, curr) => acc + curr.mileage, 0);
+  const highestMileageCar = cars.find(
+    (el) => el.mileage === Math.max(...cars.map((el) => el.mileage))
+  );
+  const lowestMileageCar = cars.find(
+    (el) => el.mileage === Math.min(...cars.map((el) => el.mileage))
+  );
+  const averageMileage = totalMileage / cars.length;
+  return { averageMileage, highestMileageCar, lowestMileageCar, totalMileage };
+}
 
 module.exports = analyzeCarMileage;
