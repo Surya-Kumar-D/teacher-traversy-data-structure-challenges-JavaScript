79474f4656145a3552418f142f2137edf5e062b1
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-09 16:33:13 +0530

    challenge 07 complete valid-ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,10 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const splitStr = str.split(".");
+  if (splitStr.length !== 4) return false;
+  return splitStr.every((el) => {
+    const num = parseInt(el);
+    return num >= 0 && num <= 255 && el === num.toString();
+  });
+};
 
 module.exports = isValidIPv4;
