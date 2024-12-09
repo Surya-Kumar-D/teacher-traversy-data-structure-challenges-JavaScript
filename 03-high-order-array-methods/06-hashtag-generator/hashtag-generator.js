629304b86ee79ddfd7b68016438c53149f56e7e3
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-09 13:27:59 +0530

    challenge 06 hastag-generator complete

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,14 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length > 140 || str.length < 1) return false;
+  return (
+    "#" +
+    str
+      .trim()
+      .split(" ")
+      .filter((el) => el.length > 0)
+      .map((el) => `${el[0]?.toUpperCase()}${el.slice(1)}`)
+      .join("")
+  );
+}
 
 module.exports = generateHashtag;
