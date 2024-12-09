4cb26dfe800ba7942dee24fc81ff16f9b79fc731
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-09 22:35:39 +0530

    challenge 10 complete find-missing-letter-with higher order function  03-high-order-methods complete

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const missingCharCode = arr.filter((char, i) => {
+    if (i === 0) {
+      return false;
+    }
+    const prevCharCode = arr[i - 1].charCodeAt(0);
+    const currentCharCode = char.charCodeAt(0);
+    return currentCharCode - prevCharCode > 1;
+  })[0];
+  return missingCharCode
+    ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1)
+    : "";
+}
 
 module.exports = findMissingLetter;
