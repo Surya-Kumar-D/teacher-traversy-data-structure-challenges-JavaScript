fa6636645520524b8af4de64b9e72cda98ba0bb1
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-10 16:04:29 +0530

    challenge 03-reverse-string complete

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,7 @@
-function reverseString() {}
+function reverseString(str) {
+  const n = str.length;
+  if (n === 0) return "";
+  return str[str.length - 1] + reverseString(str.slice(0, -1));
+}
 
 module.exports = reverseString;
