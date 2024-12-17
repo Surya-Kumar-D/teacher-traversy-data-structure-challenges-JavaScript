3336b68c124f7aa513a2ee85867284851aef2257
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-12-17 14:57:42 +0530

    challenge fibonacci and factorial complete

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,7 @@
-function factorial() {}
+function factorial(n) {
+  if (n === 0) return 1;
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
 
 module.exports = factorial;
--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,3 @@
-function power() {}
+function power(b, e) {}
 
 module.exports = power;
